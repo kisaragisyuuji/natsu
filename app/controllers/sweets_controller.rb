421128ef7acf3497b2b7f9d9d87b381a15f1ded2
kisaragisyuuji <kairasuof03@yahoo.co.jp>
2020-01-05 13:53:11 +0900

    コントローラーの設定

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -8,10 +8,22 @@ class SweetsController < ApplicationController
   end
 
   def new
+    @sweet = Sweet.new
   end
 
   def create
+    @sweet = Sweet.new(sweet_params)
+    if sweet.save
+      flash[:notice] = '投稿しました'
+      redirect_to sweets_path
+    else
+      flash.now[:alert] = '投稿できませんでした'
+      render :new
+    end
   end
   
+  def sweet_params
+    params.require(:sweet).permit(:name, :image, :descripition).merge(user_id: current_user.id)
+  end
 
 end
