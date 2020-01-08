0c728c196a6e9973f1fa9a6f40031192da7dc45a
kisaragisyuuji <kairasuof03@yahoo.co.jp>
2020-01-08 12:42:30 +0900

    コメントフォーム

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :sweets
+  resources :sweets do
+    resources :comments, only: :create
+  end
   get '/top', to: "sweet#top"
   root to: 'sweets#top'
 end
