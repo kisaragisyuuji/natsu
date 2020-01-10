7c881bf39453d3bec7635ba48a8b7d755271baab
kisaragisyuuji <kairasuof03@yahoo.co.jp>
2020-01-10 12:54:40 +0900

    ビュー

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
  def create
   @comment = Comment.create(comment_params)
-  redirect_to sweet_path(comment.sweet.id)
+  redirect_to sweet_path(@comment.sweet.id)
  end
 
  private
