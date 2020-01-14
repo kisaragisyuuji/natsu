c7c1432fa95726905371115bd892725c6ec63e0a
kisaragisyuuji <kairasuof03@yahoo.co.jp>
2020-01-14 12:18:06 +0900

    検索機能

--- app/models/sweet.rb
+++ app/models/sweet.rb
@@ -2,4 +2,9 @@ class Sweet < ApplicationRecord
   belongs_to :user
   has_many   :comments
   mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    return Sweet.all unless search
+    Sweet.where('name LIKE(?)', "%#{search}%")
+  end
 end
